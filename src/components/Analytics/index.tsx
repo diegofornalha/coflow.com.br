f8084d047a863671b09ca33bc7927cea8549f3e3
Stackbit Code Editor <projects@stackbit.com>
2023-01-28 04:02:15 +0000

    index.tsx: changed

--- src/components/Analytics/index.tsx
+++ src/components/Analytics/index.tsx
@@ -3,9 +3,25 @@ import * as React from 'react';
 export default function Analytics () {
 
     return (
-        <>
+    <>
+        <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=GTM-PZD8Q99`}
 
-        </>
+        />
+        <Script
+            dangerouslySetInnerHTML={{
+                __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'GTM-PZD8Q99', {
+                            page_path: window.location.pathname,
+                        });
+                    `,
+            }}
+        />
+    </>
     )
 
 }

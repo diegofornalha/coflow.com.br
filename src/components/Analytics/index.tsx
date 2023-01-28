d2fb4ca0399303514294a097f5c92c09a78de6d9
Stackbit Code Editor <projects@stackbit.com>
2023-01-28 04:25:31 +0000

    index.tsx: changed

--- src/components/Analytics/index.tsx
+++ src/components/Analytics/index.tsx
@@ -7,7 +7,7 @@ const Analytics = () => {
         <>
             <Script
                 async
-                src={`https://www.googletagmanager.com/gtag/js?id=GTM-PZD8Q99`}
+                src={`https://www.googletagmanager.com/gtag/js?id=G-GC2P2Z5BYT`}
 
             />
             <Script

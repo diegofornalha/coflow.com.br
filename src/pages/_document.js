e3f5eb29452fb7a82eafee3a7dc0032e2fdd5497
Stackbit Code Editor <projects@stackbit.com>
2023-01-28 03:46:53 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
     return (
-        <Html lang=''>
+        <Html lang='pt-BR'>
             <Head />
             <body>
                 <Main />
